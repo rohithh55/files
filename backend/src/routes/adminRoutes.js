cbc21ca5e0d7c263f54afea558435127113e183d
rohithh55 <rohiithh5@gmail.com>
2026-05-20 12:39:53 +0530

    Create adminRoutes.js

---
+++ backend/src/routes/adminRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getStats
+} = require("../controllers/adminController");
+
+router.get("/stats", getStats);
+
+module.exports = router;
