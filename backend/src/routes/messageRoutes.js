530b703554d929e50143a89ebcd140b4d6be0def
rohithh55 <rohiithh5@gmail.com>
2026-05-20 12:37:39 +0530

    Add message routes for creating messages

---
+++ backend/src/routes/messageRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  createMessage
+} = require("../controllers/messageController");
+
+router.post("/", createMessage);
+
+module.exports = router;
